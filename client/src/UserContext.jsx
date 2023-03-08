f377ac001a4130a7b910d55a2afa1c7b86710b03
harsh661 <plasmaharsh@gmail.com>
2023-03-08 15:29:17 +0530

    Added automatic theme selection

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useState } from "react";
 
+
 export const UserContext = createContext({})
 
+ const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches
  export function UserContextProvider({children}) {
     const [userInfo, setUserInfo] = useState(null)
-    const [darkMode, setDarkMode] = useState(false)
+    const [darkMode, setDarkMode] = useState(isDark)
 
     return (
         <UserContext.Provider value={{userInfo, setUserInfo, darkMode, setDarkMode,}}>
